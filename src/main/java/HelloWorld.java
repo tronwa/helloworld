1ffbbfbfa2bca3af2ce42d9e60194d98e2c4ccf6
tronwa <tron@telespor.no>
2021-01-27 21:52:43 +0100

    create uberjar

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,9 +1,12 @@
-/**
+/*
  * Created by jt on 2018-11-26.
  */
+import org.apache.commons.lang3.StringUtils;
+
 public class HelloWorld {
 
     public static void main(String[] args) {
         System.out.println("Hello World!!!! ");
+        System.out.println(StringUtils.capitalize("hello, my man!"));
     }
 }
